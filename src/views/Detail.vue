da8338082b8f75a00daafd41127dfab78883ad54
XCLHove <10244705+xclhove@user.noreply.gitee.com>
2024-04-20 19:44:57 +0800

    feat: 个人信息页面添加复制token的功能
    1. 个人信息页面添加复制access_token的功能
    2. 个人信息页面添加复制refresh_token的功能

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -3,8 +3,25 @@ import { storeToRefs } from "pinia";
 import { useUserStore } from "@/stores/useUserStore.ts";
 import { User } from "@/interfaces/User.ts";
 import { showPlatform } from "../interfaces/Platform.ts";
+import { ref, watch } from "vue";
+import { accessTokenUtil } from "@/utils/accessTokenUtil";
+import { refreshTokenUtil } from "@/utils/refreshTokenUtil";
+import { useClipboard } from "@vueuse/core";
+import { elPrompt } from "@/utils/elPrompt";
 
 const { user }: { user: User } = storeToRefs(useUserStore());
+
+const token = ref({
+  access: accessTokenUtil.get() || "",
+  refresh: refreshTokenUtil.get() || "",
+});
+
+const { copy: _copy, copied } = useClipboard();
+const copy = (text: string) => {
+  _copy(text).then(() => {
+    elPrompt.success("复制成功!");
+  });
+};
 </script>
 
 <template>
@@ -23,6 +40,18 @@ const { user }: { user: User } = storeToRefs(useUserStore());
       <el-text>
         {{ showPlatform(user?.preferences.providers[0].platform) }}
       </el-text>
+    </div>
+    <div class="detail-item">
+      <el-text class="label">access_token：</el-text>
+      <el-button class="token-text" type="text" @click="copy(token.access)">{{
+        token.access
+      }}</el-button>
+    </div>
+    <div class="detail-item">
+      <el-text class="label">refresh_token：</el-text>
+      <el-button class="token-text" type="text" @click="copy(token.refresh)">{{
+        token.refresh
+      }}</el-button>
     </div>
   </div>
 </template>
@@ -44,13 +73,24 @@ const { user }: { user: User } = storeToRefs(useUserStore());
     margin: 10px 0;
     width: 100%;
     border-bottom: 1px solid var(--el-border-color);
+    display: flex;
 
     * {
       font-size: 20px;
     }
 
     .label {
-      width: 50px;
+      width: fit-content();
+    }
+
+    .token-text {
+      width: 500px;
+      white-space: nowrap;
+      overflow: hidden;
+    }
+
+    .token-text:hover {
+      text-decoration: 1px solid var(--el-border-color);
     }
   }
 }
