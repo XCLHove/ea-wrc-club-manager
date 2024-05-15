3ec5155fe97fbe6054ec66905339c69e0d4ad5ad
XCLHove <10244705+xclhove@user.noreply.gitee.com>
2024-05-15 23:21:25 +0800

    refactor: 修改electron-env.d.ts
    1. 修改electron-env.d.ts

--- electron/electron-env.d.ts
+++ electron/electron-env.d.ts
@@ -1,5 +1,36 @@
 /// <reference types="vite-plugin-electron/electron-env" />
 
+interface TokenApi {
+  /**
+   * 当刷新令牌发生变化时
+   * @param callback 发生变化时的回调
+   * @return removeListener 取消监听的函数
+   */
+  addListener: (callback: (cookie: Electron.Cookie) => void) => () => void;
+  /**
+   * 移除cookie中的刷新令牌
+   */
+  remove: () => Promise<void>;
+  /**
+   * 获取新的刷新令牌和访问令牌
+   * @param refreshToken 旧的刷新令牌
+   */
+  refresh: (refreshToken: string) => Promise<{
+    accessToken: string;
+    refreshToken: string;
+  }>;
+  /**
+   * 打开登录窗口
+   * @param closeCallback 登录窗口关闭时的回调
+   * @return closeLoginWindow 关闭登录窗口的函数
+   */
+  openLoginWindow: (closeCallback?: () => void) => () => void;
+}
+
+interface SystemApi {
+  openFolder: (path?: string) => void;
+}
+
 declare namespace NodeJS {
   interface ProcessEnv {
     DIST: string;
@@ -7,4 +38,7 @@ declare namespace NodeJS {
   }
 }
 
-interface Window {}
+interface Window {
+  tokenApi: TokenApi;
+  systemApi: SystemApi;
+}
--- electron/preloadApis/systemApi.d.ts
+++
@@ -1,9 +0,0 @@
-interface SystemApi {
-  openFolder: (path?: string) => void;
-}
-
-declare global {
-  interface Window {
-    systemApi: SystemApi;
-  }
-}
